334ccaeb437b39ae8e2179d739a9e49041aae2d2
lvfwk <joaorodrigooliver9@gmail.com>
2024-12-05 19:40:46 -0300

    ..

---
+++ Revisao03.py
@@ -0,0 +1,52 @@
+# dic = {}
+# while True:
+#     nome = input("Digite seu nome: ")
+#     idade = int(input("Digite sua idade: "))
+#     dic[nome] = idade
+#     esc = input("Deseja continuar?: ")
+
+#     if esc == "sim":
+#         continue
+
+#     elif esc == "nao":
+#         break
+
+#     else:
+#         print("Por favor digite 'sim' ou 'nao' ")
+#         continue
+
+# esc = input("Digite o nome: ")
+# buscar = dic.get(esc)
+# print(buscar)
+
+#====================================================#
+lista = [1 , 2 , 2, 4, 4, 5, 6, 7, 7, 8, 8, 9, 3]
+lista2 = []
+
+for i in lista:
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
